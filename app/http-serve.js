65e140229be55cf53c9617b223163789ec72116a
i5ting <i5ting@126.com>
2017-01-06 10:16:00 +0800

    lazycommmit, please ignore

---
+++ app/http-serve.js
@@ -0,0 +1,10 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic('public/ftp', {'index': ['index.html', 'index.htm']})
+
+module.exports = function onRequest (req, res) {
+  serve(req, res, finalhandler(req, res))
+}
